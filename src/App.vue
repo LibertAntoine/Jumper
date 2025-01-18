502a1a4703b590aa5a1881b5b54b77e8131e9355
Antoine Libert <antoine-libert@outlook.com>
2025-01-18 16:28:18 +0100

    first commit : setup

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Button from '@@materials/ui/button/Button.vue'
+</script>
+
+<template>
+  <main class="text-red-500">
+    <Button >Hello</Button>
+    World
+  </main>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from 'url'
+
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@@types': fileURLToPath(new URL('./src/@types', import.meta.url)),
+      '@@materials': fileURLToPath(new URL('./src/components/@materials', import.meta.url)),
+      '@@common': fileURLToPath(new URL('./src/components/@common', import.meta.url)),
+    },
+  },
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
